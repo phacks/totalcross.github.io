81702dcdd3d9dd8a0cec028f51a2a9a504e80949
Nathalie <nmarquesin@live.com>
2020-09-03 16:06:46 +0100

    Edit branch from master to docs-site

--- prepare-docs.sh
+++ prepare-docs.sh
@@ -4,4 +4,4 @@ WORKDIR=$(cd $BASEDIR; pwd)
 #wipe src/docs/md
 rm -rf "${WORKDIR}/src/docs/md"
 # update t
-git clone https://github.com/TotalCross/docs --depth=1 --branch=master "${WORKDIR}/src/docs/md"
+git clone https://github.com/TotalCross/docs --depth=1 --branch=docs-site "${WORKDIR}/src/docs/md"
